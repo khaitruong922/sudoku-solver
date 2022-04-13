838ccc922f825454c96f8c9962d2db86bbec9b15
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-04-14 03:07:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from src.boards import *
 
 if __name__ == '__main__':
-    s5 = sudoku_evil().solve_and_display()
-    s5.display_candidates()
+    sudoku_evil().solve_and_display().display_candidates()
